f2090adec674e0f4afe39714fcc8e5429c693cec
rutuja.kadam <rutujakadam0204@gmail.com>
2023-10-02 21:00:58 +0530

    celerry file name change

--- examination_project/celerry.py
+++ examination_project/celerry.py
@@ -1,6 +1,6 @@
 
 import os
-from celerry import Celery
+from celery import Celery
   
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'examination_project.settings')
